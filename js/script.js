779d6722574d681db4de8690ae804cf3cf301ff4
ocamj015 <joaquin.x.ocampo.-nd@disney.com>
2016-06-03 17:31:03 -0300

    andando console.log

--- js/script.js
+++ js/script.js
@@ -11,29 +11,52 @@
 //         console.log(data);
 //     });
 // });
-$(document).ready(function() {
-    var url = 'http://api.themoviedb.org/3/',
-        mode = 'search/movie?query=',
-        input,
-        movieName,
-        key = '&api_key=43fa16adddb5c45562182359a3c3f902';
+// $(document).ready(function() {
+//     var url = 'http://api.themoviedb.org/3/',
+//         mode = 'search/movie?query=',
+//         input,
+//         movieName,
+//         key = '&api_key=43fa16adddb5c45562182359a3c3f902';
 
-    $('button').click(function() {
-        var input = $('#movie').val(),
-            movieName = encodeURI(input);
-        $.ajax({
-            type: 'GET',
-            url: url + mode + input + key,
-            async: false,
-            jsonpCallback: 'testing',
-            contentType: 'application/json',
-            dataType: 'jsonp',
-            success: function(json) {
-                console.dir(json);
-            },
-            error: function(e) {
-                console.log(e.message);
-            }
-        });
-    });
-});​
+//     $('button').click(function() {
+//         $.getJSON(url + mode + input + key, function(json) {
+//             alert();
+//             console.log(json);
+//         });
+//         // success: function(json) {
+//         //     console.dir(json);
+//         //     console.log(json);
+//         //     alert();
+//         //     $('.peli').html('Hello World!');
+//         // },
+//         // error: function(e) {
+//         //     console.log(e.message);
+//         //     alert();
+//         // }
+//     });
+// });
+
+
+
+
+
+
+$(document).ready(function() {
+   var url = 'http://api.themoviedb.org/3/',
+       mode = 'search/movie?query=',
+       input,
+       movieName,
+       key = '&api_key=43fa16adddb5c45562182359a3c3f902';
+   $('#searchButton').click(function() {
+console.log('entr');
+       var input = $('#filter').val(),
+urlCompleta = url + mode + input + key;
+console.log(urlCompleta);
+       $.getJSON(
+           'http://api.themoviedb.org/3/search/movie?query=lord&api_key=43fa16adddb5c45562182359a3c3f902',
+           function(json) {
+               console.log(json);
+           }
+       );
+   });
+});
