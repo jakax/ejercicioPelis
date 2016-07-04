af43e0d911512d9bc6c7eb3dc43272ae0076214d
ocamj015 <joaquin.x.ocampo.-nd@disney.com>
2016-07-04 17:40:05 -0300

    mostrar por consola la busqueda con angularJS

--- js/script.js
+++ js/script.js
@@ -37,65 +37,102 @@
 // });
 
 
-var app = angular.module(‘store’, [ ]);
+// var app = angular.module(‘’, []);
 
-app.controller(‘FilmController’, function(){
-    this.product = films;
-});
+// app.controller(‘PelisController’, function(){
+//     this.product = films;
+// });
 
-var url = 'http://api.themoviedb.org/3/',
-    mode = 'search/movie?query=',
-    input,
-    movieName,
-    key = '&api_key=43fa16adddb5c45562182359a3c3f902';
-    urlCompleta = url + mode + input + key;
-
-
-$http({method: 'GET', url: '/someUrl'}).success(function(data, status, headers, config) {
-    var mainInfo = $http.get('content.json').success(function(response) {
-        return response.data;
-    });
-
-app.factory('mainInfo', function($http) { 
-
-    var obj = {content:null};
-
-    $http.get('content.json').success(function(data) {
-        // you can do some processing here
-        obj.content = data;
-    });    
-
-    return obj;    
-});
+// var url = 'http://api.themoviedb.org/3/',
+//     mode = 'search/movie?query=',
+//     input,
+//     movieName,
+//     key = '&api_key=43fa16adddb5c45562182359a3c3f902';
+//     urlCompleta = url + mode + input + key;
 
 
+// $http({method: 'GET', url: urlCompleta}).success(function(data, status, headers, config) {
+//     var mainInfo = $http.get('content.json').success(function(response) {
+//         return response.data;
+//     });
+
+// app.factory('mainInfo', function($http) { 
+
+//     var obj = {content:null};
+
+//     $http.get('content.json').success(function(data) {
+//         // you can do some processing here
+//         obj.content = data;
+//     });    
+
+//     return obj;    
+// });
 
 
+// function searchFunction() {
+//     $(document).ready(function() {
+//         var url = 'http://api.themoviedb.org/3/',
+//             mode = 'search/movie?query=',
+//             input,
+//             movieName,
+//             key = '&api_key=43fa16adddb5c45562182359a3c3f902';
+//         $('#searchButton').click(function() {
+//             console.log('entr');
+//             var input = $('#filter').val(),
+//                 urlCompleta = url + mode + input + key;
+//                 console.log(urlCompleta);
+//             $.getJSON(
+//                 urlCompleta,
+//                 //'http://api.themoviedb.org/3/search/movie?query=lord&api_key=43fa16adddb5c45562182359a3c3f902',
+//                 function(json) {
+//                     function takeName (element, idex, array) {
+//                         console.log("el titulo de la pelicula es: " + element.original_title);
+//                     }
+//                     var films = json.results;
+//                     console.log(json.results);
+//                     films.forEach(takeName);
+//                     //var obj = JSON.parse(json);
+//                     document.getElementById("demo").innerHTML = resultadoDeLaBusqueda.forEach(takeName);
+//                 }
+//             );
+//         });
+//     });
+// }
 
-$(document).ready(function() {
-    var url = 'http://api.themoviedb.org/3/',
-        mode = 'search/movie?query=',
-        input,
-        movieName,
-        key = '&api_key=43fa16adddb5c45562182359a3c3f902';
-    $('#searchButton').click(function() {
-console.log('entr');
-        var input = $('#filter').val(),
-urlCompleta = url + mode + input + key;
-console.log(urlCompleta);
+
+angular.module('myApp', [])
+.controller('miControlador', ['$scope', function($scope) {
+    var self = this;
+    self.searchFunction = function() {
+        var url = 'http://api.themoviedb.org/3/',
+            mode = 'search/movie?query=',
+            movieName,
+            key = '&api_key=43fa16adddb5c45562182359a3c3f902',
+
+            urlCompleta = url + mode + self.entrada + key;
+            console.log(urlCompleta); 
+            
         $.getJSON(
             urlCompleta,
-           //'http://api.themoviedb.org/3/search/movie?query=lord&api_key=43fa16adddb5c45562182359a3c3f902',
+            //'http://api.themoviedb.org/3/search/movie?query=lord&api_key=43fa16adddb5c45562182359a3c3f902',
             function(json) {
                 function takeName (element, idex, array) {
                     console.log("el titulo de la pelicula es: " + element.original_title);
+                    var titulo = element.original_title;
+                    //$scope.titulos;
                 }
-                var films = json.results;
-                console.log(json.results);
-                films.forEach(takeName);
+                var resultadoDeLaBusqueda = json.results;
+                //$scope.films;
+                $scope.titulos = resultadoDeLaBusqueda.forEach(takeName);
                 //var obj = JSON.parse(json);
-                document.getElementById("demo").innerHTML = resultadoDeLaBusqueda.forEach(takeName);
-            }
-        );
-    });
-});
+                //document.getElementById("demo").innerHTML = resultadoDeLaBusqueda.forEach(takeName);
+                }
+            );
+    };
+}]);
+
+
+
+
+
+
